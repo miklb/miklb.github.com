5f49c71624e8fa9de0c7dcc84093d62bc1bcfb42
Michael Bishop <miklb@miklb.com>
2016-01-30 03:10:42 -0500

    another odd thought to sleep the build script 2 minutes before running rake file

--- build.sh
+++ build.sh
@@ -29,6 +29,5 @@ git config user.name "miklb"
 git add -A .
 git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
 git push --quiet origin master > /dev/null 2>&1
-do
-    rake webmention
-done
+
+sleep 2m
