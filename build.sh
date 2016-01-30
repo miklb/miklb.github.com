ee58b30156a2c16d76a597a1048e5acdf8b096b0
miklb <miklb@miklb.com>
2016-01-30 09:19:50 +0000

    Travis #53

--- build.sh
+++ build.sh
@@ -31,3 +31,9 @@ git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
 git push --quiet origin master > /dev/null 2>&1
 
 sleep 2m
+
+rake webmentions
+
+git add -A .
+git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER webmention cache"
+git push --quiet origin jekyll > /dev/null 2>&1
