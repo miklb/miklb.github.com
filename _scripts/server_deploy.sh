d782fd42d32fc97105031547f1b050728717e5c5
Michael Bishop <miklb@miklb.com>
2016-08-18 02:04:51 -0400

    see where failing now

--- _scripts/server_deploy.sh
+++ _scripts/server_deploy.sh
@@ -24,4 +24,4 @@ git status
 git show-ref
 git add --ignore-removal .
 git commit -a -m "Webmentions Travis #$TRAVIS_BUILD_NUMBER"
-git push --quiet "https://${GH_TOKEN}@${GH_REF}" origin:jekyll > /dev/null 2>&1
+git push "https://${GH_TOKEN}@${GH_REF}" origin:jekyll
