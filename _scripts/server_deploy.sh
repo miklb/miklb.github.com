4332446ab0416720bc160a0df5048f00f7d691d4
Michael Bishop <miklb@miklb.com>
2016-08-18 04:44:06 -0400

    yeah, don't commit all, after adding some

--- _scripts/server_deploy.sh
+++ _scripts/server_deploy.sh
@@ -23,8 +23,8 @@ cd ../
 git checkout -b jekyll_new
 git status
 git show-ref
-git add --no-all .
-git commit -a -m "Webmentions Travis #$TRAVIS_BUILD_NUMBER"
+git add --ignore-removal .
+git commit -m "Webmentions Travis #$TRAVIS_BUILD_NUMBER"
 git checkout jekyll
 git merge jekyll_new
 git push --quiet "https://${GH_TOKEN}@${GH_REF}" HEAD:jekyll > /dev/null 2>&1
